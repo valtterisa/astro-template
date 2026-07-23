2abc3522d85bdde0fb00fd99bbe7518980f20b58
Valtteri <savonen.emppu@gmail.com>
2026-07-23 19:39:08 +0300

    Update Astro configuration to enhance development setup. Added server settings for host and port, configured Vite to allow hosts and enable CORS, and disabled the development toolbar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: { host: true, port: 4321 },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: { allowedHosts: true, cors: true },
+  },
+  devToolbar: {
+    enabled: false
   }
 });
